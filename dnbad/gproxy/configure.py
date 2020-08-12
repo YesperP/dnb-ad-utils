53efb872542e63671a54bab359666ba59f6e4732
Jesper Pedersen <Jesper.Pedersen@dnb.no>
2020-08-12 16:44:05 +0200

    Fixed configuring ssh

--- dnbad/gproxy/configure.py
+++ dnbad/gproxy/configure.py
@@ -5,7 +5,7 @@ from sshconf import read_ssh_config, empty_ssh_config_file
 
 from dnbad.common.configure import *
 from . import *
-from .util import show_line_diff
+from .util import show_line_diff, show_file
 
 
 def configure():
@@ -41,32 +41,39 @@ def _configure_openssh():
     host_name = BIND_HOST
 
     if os.path.exists(SSH_CONFIG_PATH):
+        ssh_config = read_ssh_config(SSH_CONFIG_PATH)
 
-        # Create a compliant config:
-        new_config = read_ssh_config(SSH_CONFIG_PATH)
-        for key, val in DEFAULT_BIT_BUCKET_HOST.items():
-            new_config.set(host_name, **{key: val})
-        new_config_str = new_config.config()
+        # Check and create a compliant config:
+        changed = False
+        host_config = ssh_config.host(host_name)
+        if len(host_config) == 0:
+            changed = True
+            ssh_config.add(host_name, **DEFAULT_BIT_BUCKET_HOST)
+        else:
+            for key, val in DEFAULT_BIT_BUCKET_HOST.items():
+                if host_config.get(key.lower(), "").lower() != val.lower():
+                    changed = True
+                    ssh_config.set(host_name, **{key: val})
 
         # Old config:
-        old_config_str = read_ssh_config(SSH_CONFIG_PATH).config()
+        if changed:
+            new_config_str = ssh_config.config()
+            old_config_str = read_ssh_config(SSH_CONFIG_PATH).config()
 
-        if new_config_str != old_config_str:
-            print(f"Some changes are needed for your ssh config file ({SSH_CONFIG_PATH}).")
-            if yes_no("Do you want to look at the changes?", default=True):
-                show_line_diff(
-                    old=old_config_str.split("\n"),
-                    new=new_config_str.split("\n")
-                )
+            print(f"Some changes are needed for your ssh config file ({SSH_CONFIG_PATH}):")
+            show_line_diff(
+                old=old_config_str.split("\n"),
+                new=new_config_str.split("\n")
+            )
             if yes_no("Do you want us to make these changes?", default=True):
-                new_config.save(SSH_CONFIG_PATH)
+                ssh_config.save()
         else:
             print("Your SSH config file is already configured correctly. No changes needed.")
     else:
         ssh_config = empty_ssh_config_file()
-        for key, val in DEFAULT_BIT_BUCKET_HOST.items():
-            ssh_config.set(host_name, **{key: val})
-        print(f"No ssh config found at {SSH_CONFIG_PATH}")
+        ssh_config.add(host_name, **DEFAULT_BIT_BUCKET_HOST)
+        print(f"No ssh config found at {SSH_CONFIG_PATH}. We need to create this file:")
+        show_file(ssh_config.config().split("\n"))
         if yes_no("Do you want us to create the file?", default=True):
             ssh_config.write(SSH_CONFIG_PATH)
 
--- dnbad/gproxy/util.py
+++ dnbad/gproxy/util.py
@@ -1,23 +1,43 @@
 import socket
 from typing import List
+import itertools
+
+
+_MIN_HEADER_OLD = "--OLD--"
+_MIN_HEADER_NEW = "--NEW--"
 
 
 def show_line_diff(old: List[str], new: List[str]):
-    line_max_old = max(len(l) for l in old)
-    line_max_new = max(len(l) for l in new)
-    f_str = f"{{}} |{{:<{line_max_old}}}|{{:<{line_max_new}}}"
+    width_max_old = max((len(line) for line in old), default=0)
+    width_max_new = max((len(line) for line in new), default=0)
 
-    old.append("".join(["-"] * line_max_old))
-    new.append("".join(["-"] * line_max_new))
+    width_max_old = max(len(_MIN_HEADER_OLD), width_max_old)
+    width_max_new = max(len(_MIN_HEADER_NEW), width_max_new)
 
-    longest = old if len(old) > len(new) else new
-    shortest = new if len(old) > len(new) else old
-    shortest.extend([" "] * (len(longest) - len(shortest)))
+    old.append("".center(width_max_old, "-"))
+    new.append("".center(width_max_new, "-"))
 
-    sep = "".join(["-"] * 4)
-    print(f_str.format(" ", f"{sep} OLD {sep}", f"{sep} NEW {sep}"))
-    for left_line, right_line in zip(old, new):
-        print(f_str.format("X" if left_line != right_line else " ", left_line, right_line))
+    f_str = "{}|{}|{}|"
+
+    print(f_str.format("--", _MIN_HEADER_OLD.center(width_max_old, "-"), _MIN_HEADER_NEW.center(width_max_new, "-")))
+    for i, (left_line, right_line) in enumerate(itertools.zip_longest(old, new, fillvalue=None)):
+        if i + 1 == max(len(old), len(new)):
+            marker = "--"
+        elif left_line == right_line:
+            marker = "  "
+        else:
+            marker = "X "
+        print(f_str.format(marker, (left_line or "").ljust(width_max_old), (right_line or "").ljust(width_max_new)))
+
+
+def show_file(lines: List[str]):
+    line_max = max((len(line) for line in lines), default=0)
+    line_max = max(line_max, len(_MIN_HEADER_NEW))
+    f_str = "|{}|"
+    print(f_str.format(_MIN_HEADER_NEW.center(line_max, "-")))
+    for line in lines:
+        print(f_str.format(line.ljust(line_max)))
+    print(f_str.format("".center(line_max, "-")))
 
 
 # noinspection PyBroadException
